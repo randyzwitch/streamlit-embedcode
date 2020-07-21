5cdc06f32ddaa7ea323d58b7fb4aa38bf2231ae7
Randy Zwitch <rzwitch@gmail.com>
2020-07-21 16:49:08 -0400

    Add majority of tests

--- tests/test_package.py
+++ tests/test_package.py
@@ -20,18 +20,39 @@ def test_cleanlink_trailing():
     )
 
 
-class MyTestClass(BaseCase):
+class ComponentsTest(BaseCase):
     def test_basic(self):
 
+        # open the app
         self.open("http://localhost:8501")
-        self.assert_title("app_to_test · Streamlit")
-        # self.type('input[name="q"]', "xkcd book\n")
-        # self.assert_text("xkcd: volume 0", "h3")
-        # self.open("https://xkcd.com/353/")
 
-        # self.assert_element('img[alt="Python"]')
-        # self.click('a[rel="license"]')
-        # self.assert_text("free to copy and reuse")
-        # self.go_back()
-        # self.click_link_text("About")
-        # self.assert_exact_text("xkcd.com", "h2")
+        # check basic properties of all Streamlit apps
+        self.assert_title("app_to_test · Streamlit")
+        self.assert_element("div.withScreencast")
+        self.assert_element("div.stApp")
+
+        # github_gist
+        self.assert_text("github_gist test")
+        self.is_element_in_an_iframe("div.gist-file")
+        self.is_element_in_an_iframe("div.gist-data")
+        self.is_element_in_an_iframe("div.gist-meta")
+
+        # gitlab_snippet
+        self.assert_text("gitlab_snippet test")
+        self.is_element_in_an_iframe("div.gitlab-embed-snippets")
+
+        # pastebin_snippet
+        self.assert_text("pastebin_snippet test")
+        self.is_element_in_an_iframe("div.embedPastebin")
+
+        # codepen_snippet
+        self.assert_text("codepen_snippet test")
+        self.is_element_in_an_iframe("div.cp_embed_wrapper")
+
+        # ideone_snippet
+        self.assert_text("ideone_snippet test")
+        # TODO
+
+        # tagmycode_snippet
+        self.assert_text("tagmycode_snippet test")
+        self.is_element_in_an_iframe("div#tmc-embed-snippet")
