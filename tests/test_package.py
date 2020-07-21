e784d228041320f1ee61f4e5b1e914afd320db05
Randy Zwitch <rzwitch@gmail.com>
2020-07-21 14:58:09 -0400

    Remove stale example code

--- tests/test_package.py
+++ tests/test_package.py
@@ -19,34 +19,7 @@ def test_cleanlink_trailing():
     )
 
 
-# # set up Selenium
-# # https://www.lambdatest.com/blog/test-automation-using-pytest-and-selenium-webdriver/
-# import pytest
-# import subprocess
-# from selenium import webdriver
-# from chromedriver_py import binary_path
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.keys import Keys
-
-# # Fixture for Chrome
-# @pytest.fixture(scope="class")
-# def chrome_driver_init(request):
-#     chrome_driver = webdriver.Chrome(executable_path=binary_path)
-#     request.cls.driver = chrome_driver
-#     yield
-#     chrome_driver.close()
-
-
-# @pytest.mark.usefixtures("chrome_driver_init")
-# class BasicTest:
-#     pass
-
-
-# class Test_URL(BasicTest):
-#     def test_open_url(self):
-#         self.driver.get("http://testHost")
-#         print(self.driver.title)
-
+# https://seleniumbase.io/
 from seleniumbase import BaseCase
 
 
