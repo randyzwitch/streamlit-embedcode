dd0af1fc43f642721a96fbbd2af7747520ba8b8b
Randy Zwitch <rzwitch@gmail.com>
2020-07-21 15:17:29 -0400

    Run app in background

--- tests/app_to_test.py
+++ tests/app_to_test.py
@@ -1,1 +1,5 @@
 import streamlit as st
+from streamlit_embedcode import github_gist
+
+st.title("github_gist test")
+github_gist("https://gist.github.com/randyzwitch/934d502e53f2adcb48eea2423fe4a47e")
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,4 +1,5 @@
 from streamlit_embedcode import _clean_link
+from seleniumbase import BaseCase  # https://seleniumbase.io/
 
 
 def test_cleanlink_notrailing():
@@ -19,20 +20,18 @@ def test_cleanlink_trailing():
     )
 
 
-# https://seleniumbase.io/
-from seleniumbase import BaseCase
-
-
 class MyTestClass(BaseCase):
     def test_basic(self):
-        self.open("https://store.xkcd.com/search")
-        self.type('input[name="q"]', "xkcd book\n")
-        self.assert_text("xkcd: volume 0", "h3")
-        self.open("https://xkcd.com/353/")
-        self.assert_title("xkcd: Python")
-        self.assert_element('img[alt="Python"]')
-        self.click('a[rel="license"]')
-        self.assert_text("free to copy and reuse")
-        self.go_back()
-        self.click_link_text("About")
-        self.assert_exact_text("xkcd.com", "h2")
+
+        self.open("http://localhost:8501")
+        self.assert_title("app_to_test · Streamlit")
+        # self.type('input[name="q"]', "xkcd book\n")
+        # self.assert_text("xkcd: volume 0", "h3")
+        # self.open("https://xkcd.com/353/")
+
+        # self.assert_element('img[alt="Python"]')
+        # self.click('a[rel="license"]')
+        # self.assert_text("free to copy and reuse")
+        # self.go_back()
+        # self.click_link_text("About")
+        # self.assert_exact_text("xkcd.com", "h2")
